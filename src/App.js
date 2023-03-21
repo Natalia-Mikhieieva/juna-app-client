4a7b1e4220040b88e050fe6c4c02220948a24199
Natalia <8marsy@gmail.com>
2023-03-21 14:53:25 -0400

    component, pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
 
 function App() {
-  return <div className="App">
-  JUNA app</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<CatalogPage />} />
+      </Routes>
+      JUNA app
+    </div>
+  );
 }
 
 export default App;
