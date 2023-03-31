90ca9fd9ba195ae06033fe7dd40b1ff9870dca92
Natalia <8marsy@gmail.com>
2023-03-31 16:24:23 -0400

    navbarstyle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,56 +4,72 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-<li >
-        <NavLink to="/" >
-          <img src="https://www.entrustit.co.uk/wp-content/uploads/2014/10/loaf_logo.jpeg" alt="logo-img" className="nav-logo" />
+    <nav className="navigation">
+      <div className="brand">
+        <a href="/" className="brand-name">
+          JUNA
+        </a>
+        <NavLink to="/">
+          <img
+            src="https://www.svgrepo.com/show/323714/abstract-039.svg"
+            alt="logo-img"
+            className="nav-logo"
+          />
         </NavLink>
-</li>
-<li>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? "selected" : "")}
-        >
-          Home
-        </NavLink>
-</li>
+      </div>
 
-<li>
-        <NavLink
-          to="/catalog"
-          className={({ isActive }) => (isActive ? "selected" : "")}
-        >
-          Catalog
-        </NavLink>
-</li>
-<li>
-        <NavLink
-          to="/cart"
-          className={({ isActive }) => (isActive ? "selected" : "")}
-        >
-          <img src="./cart-icon.svg" alt="cart-icon" />
-          Cart
-        </NavLink>
-</li>
-<li>
-        <NavLink
-          to="/signup"
-          className={({ isActive }) => (isActive ? "selected" : "")}
-        >
-          Signup
-        </NavLink>
-</li>
-<li>
-        <NavLink
-          to="/login"
-          className={({ isActive }) => (isActive ? "selected" : "")}
-        >
-          Login
-        </NavLink>
-</li>
-      </ul>
+      <div className="navigation-menu">
+        <ul className="navbar-list">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/catalog"
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              Catalog
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/cart"
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/60/60992.png"
+                alt="cart-icon"
+                className="cart-logo"
+              />
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/signup"
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              Signup
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
