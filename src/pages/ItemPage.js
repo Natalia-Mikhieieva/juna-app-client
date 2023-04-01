d12b90b70eed91c4ae7879a87b648d8f4df3c03c
Natalia <8marsy@gmail.com>
2023-04-01 04:58:20 -0400

    style

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ItemPage() {
   const [item, setItem] = useState([]);
@@ -11,7 +12,7 @@ function ItemPage() {
     axios
       .get(`http://localhost:5005/api/item/${id}`)
       .then((response) => {
-        console.log("response.data", response.data);
+        console.log("response.data", response)
         setItem(response.data);
       })
       .catch((err) => console.log(err));
@@ -22,16 +23,16 @@ function ItemPage() {
       <Navbar></Navbar>
       <div className="card">
         <h1> Item </h1>
-        <img src={item.imageUrl} alt="item" />
+        {/*  <img src={item.imageUrl} alt="item" />
         <h3>{item.title}</h3>
         <span>{item.brand}</span>
         <p>{item.description}</p>
         <p>{item.stock}</p>
         <p>{item.price}</p>
         <button>Edit this Item</button>
-        {/*   <Link to={`/catalog/item-edit/${item._id}`}>
-              <button>Edit this Item</button>
-            </Link> */}
+        <Link to={`/catalog/item-edit/${item._id}`}>
+          <button>Edit this Item</button>
+        </Link> */}
       </div>
     </div>
   );
